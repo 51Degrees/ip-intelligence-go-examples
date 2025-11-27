a9197291c91294dfbe36ee57b7101df8b1be925d
Marycka9 <marycka9@gmail.com>
2025-11-27 10:30:38 +0200

    Fix with go.sum hash sum dependencies

--- ci/update-packages.ps1
+++ ci/update-packages.ps1
@@ -6,9 +6,19 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 Push-Location $RepoName
 try {
+	go clean -modcache
+
+    if (Test-Path "go.sum") {
+        Remove-Item "go.sum" -Force
+    }
+
     $env:GOPROXY = "direct"
+	go mod download
+
     go get -u ./...
     go mod tidy
+
+	go mod verify
 } finally {
     Pop-Location
 }
