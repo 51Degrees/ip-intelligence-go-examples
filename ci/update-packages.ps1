dfc4fd6e819b6a231cd26cc9e823048b281d59e8
Marycka9 <marycka9@gmail.com>
2025-11-27 10:34:55 +0200

    Revert back changes; Added .gitignore parameters

--- ci/update-packages.ps1
+++ ci/update-packages.ps1
@@ -6,19 +6,9 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 Push-Location $RepoName
 try {
-	go clean -modcache
-
-    if (Test-Path "go.sum") {
-        Remove-Item "go.sum" -Force
-    }
-
     $env:GOPROXY = "direct"
-	go mod download
-
     go get -u ./...
     go mod tidy
-
-	go mod verify
 } finally {
     Pop-Location
 }
