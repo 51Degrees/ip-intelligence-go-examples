9360f2fd115ed92896be0fee6e7c844e1eb9a14b
Alexander Popel <74662418+pi-alexander-popel@users.noreply.github.com>
2025-12-03 05:37:56 +0200

    Merged Pull Request '#16 common-fetch-assets->main: ci: use common fetch-assets script'

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -1,23 +1,8 @@
-param (
-    [Parameter(Mandatory)][string]$RepoName
-)
 $ErrorActionPreference = "Stop"
 
-$assets = New-Item -ItemType Directory -Path assets -Force
-$assetsDestination = "$RepoName"
+$assets = "20000 Evidence Records.yml" #, "51Degrees-LiteV41.ipi" # TODO: uncomment light IPI file when it exists
 
-$downloads = @{
-    # "51Degrees-LiteV41.ipi" = {Invoke-WebRequest -Uri "https://github.com/51Degrees/ip-intelligence-data/raw/main/51Degrees-LiteV41.ipi" -OutFile $assets/$file} // TODO: set the right path to the file when the file will be available
-    "20000 Evidence Records.yml" = {Invoke-WebRequest -Uri "https://github.com/51Degrees/ip-intelligence-data/blob/main/evidence.yml" -OutFile $assets/$file}
+./steps/fetch-assets.ps1 -Assets $assets
+foreach ($asset in $assets) {
+    New-Item -ItemType SymbolicLink -Force -Target "$PWD/assets/$asset" -Path "$PSScriptRoot/../$asset"
 }
-
-foreach ($file in $downloads.Keys) {
-    if (!(Test-Path $assets/$file)) {
-        Write-Output "Downloading $file"
-        Invoke-Command -ScriptBlock $downloads[$file]
-    } else {
-        Write-Output "'$file' exists, skipping download"
-    }
-}
-
-New-Item -ItemType SymbolicLink -Force -Target "$assets/20000 Evidence Records.yml" -Path "$assetsDestination/20000 Evidence Records.yml"
